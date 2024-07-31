a0aeb8b8ecae6dea177af1c9582b27f0d921998e
Lora <lora.drazd@gmail.com>
2024-07-31 16:58:39 +0300

    no message

--- 3_e2e/tests/computer-database.spec.js
+++ 3_e2e/tests/computer-database.spec.js
@@ -6,6 +6,14 @@ import { ComputersListPage } from "../pages/computers-list-page";
 test("Create new computer", async ({ page }) => {
   await page.goto("https://computer-database.gatling.io/computers");
   await expect(page).toHaveTitle("Computers database");
+  await page.locator("#add").click();
+  await page.locator("#name").fill('PC');
+  await page.locator("#introduced").fill("2010-06-18");
+  await page.locator("#discontinued").fill("2020-06-18");
+  await page.locator("#introduced").fill("2010-06-18");
+  await page.locator("#company").selectOption("ASUS");
+  await page.locator('input[type="submit"]').click();
+  await expect(page.locator("div.alert-message.warning")).toContainText("Done");
 });
 
 //Task - 2: Update existing test to verify computer creation workflow with POM
@@ -15,3 +23,7 @@ test("Create new computer with page object model", async ({ page }) => {
   await computersListPage.goto();
   await computersListPage.openNewComputerCreationPage();
 });
+
+//Task -2
+
+
