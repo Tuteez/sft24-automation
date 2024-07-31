5af24032977e50a1aed2e456291bf0a82743ad7c
Sefleris <shefleris@gmail.com>
2024-07-31 16:54:13 +0300

    Unfinished task 4

--- 1_unit/tests/calculate-score-range.test.js
+++ 1_unit/tests/calculate-score-range.test.js
@@ -1,12 +1,19 @@
 const calculateScoreRange = require("../functions/calculate-score-range");
 
 describe("calculateScoreRange tests", () => {
+  test("5, 5, 7 numbers should get range 1", () => {
+    expect(calculateScoreRange(5, 5, 7)).toBe(1);
+  });
+
+  test("5, 5, 8 numbers should get range 2", () => {
+    expect(calculateScoreRange(5, 5, 8)).toBe(2);
+  });
   test("1, 2, 3 numbers should get range 1", () => {
-    expect(calculateScoreRange(1, 2, 3)).toBe(1);
+    expect(calculateScoreRange(5, 5, 40)).toBe(2);
   });
 
   test("5, 7, 10 numbers should get range 2", () => {
-    expect(calculateScoreRange(5, 7, 10)).toBe(2);
+    expect(calculateScoreRange(5, 5, 41)).toBe(3);
   });
 
   test("5, 25, 25 numbers should get range 3", () => {
