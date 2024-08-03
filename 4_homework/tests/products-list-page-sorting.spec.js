9a4a182b30de3cb63501ad5a0fcb372324acb21e
pillika <ugne.ciz@gmail.com>
2024-08-03 16:06:18 +0300

    Fixed tests startup

--- 4_homework/tests/products-list-page-sorting.spec.js
+++ 4_homework/tests/products-list-page-sorting.spec.js
@@ -2,34 +2,35 @@ import { test, expect } from "@playwright/test";
 import { LoginPage } from "../pages/login-page";
 import { ProductsListPage } from "../pages/products-list-page"
 
-const loginData = [
-    { userName: 'standard_user', password: 'secret_sauce' },
-    { userName: 'problem_user', password: 'secret_sauce' }
-];
 
-test("sss", () => {})
-      
-loginData.forEach(async ({ userName, password }) => {
-   // test.describe('SFT-1 Sorting functionality on Products list.', () => { 
+test.describe('SFT-1 Sorting functionality on Products list.', () => {
+
+    const loginData = [
+        { userName: 'standard_user', password: 'secret_sauce' },
+        { userName: 'problem_user', password: 'secret_sauce' }
+    ];
+
+    loginData.forEach(({ userName, password }) => {
+
         let loginPage;
         let productsListPage;
-        test.beforeEach('Login', async ({ page }) => {
+        test.beforeEach(async ({ page }) => {
             loginPage = new LoginPage(page)
             productsListPage = new ProductsListPage(page);
-
+    
             await loginPage.goto('/');
             await loginPage.login(userName, password);
             expect(page).toHaveURL('/inventory.html');
         })
+            
         test.afterEach(async () => {
             await page.close();
         });
-        test("Check if sorting button exists",async ({ page }) => {
-            await page.goto('/inventory.html');
-            expect(productsListPage.sortingButton).toBeVisible();
-          });
-        
-  //  });
-    
 
+
+        test(`Check if sorting button exists for user: ${userName}`, async ({ page }) => {
+         await page.goto('/inventory.html');
+            expect(productsListPage.sortingButton).toBeVisible();
+        });
+      });
 });
