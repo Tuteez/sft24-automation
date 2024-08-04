204c4871764269639113fbcede6e1a8bc0b72d75
Lora Drazdauskaitė <125921269+lora-drazdauskaite@users.noreply.github.com>
2024-08-04 12:30:36 +0300

    Revert "Kaunas/workshop lora drazdauskaite"

--- 3_e2e/tests/computer-database.spec.js
+++ 3_e2e/tests/computer-database.spec.js
@@ -6,19 +6,6 @@ import { ComputersListPage } from "../pages/computers-list-page";
 test("Create new computer", async ({ page }) => {
   await page.goto("https://computer-database.gatling.io/computers");
   await expect(page).toHaveTitle("Computers database");
-  //click add new pc
-  await page.locator("#add").click();
-  //fill name
-  await page.locator("#name").fill('kompas');
-  //date1
-  await page.locator("#introduced").fill('2001-01-01');
-  //date2
-  await page.locator("#discontinued").fill('2009-09-09');
-  //select company
-  await page.locator("#company").selectOption('RCA');
-  //submit button
-  await page.locator('input[type="submit"]').click();
-  await expect(page.locator("div.alert-message.warning")).toContainText("Done");
 });
 
 //Task - 2: Update existing test to verify computer creation workflow with POM
@@ -27,41 +14,4 @@ test("Create new computer with page object model", async ({ page }) => {
 
   await computersListPage.goto();
   await computersListPage.openNewComputerCreationPage();
-  await computersListPage.submitComputerDetails("kompas","2001-01-01","2019-10-10",'RCA');
-  await computersListPage.confirmSubmittion();
-
 });
-
-//task - 3 verify search feature with no data
-
-test("Verify search feature with no data", async ({page}) => {
-  let computersListPage = new ComputersListPage(page);
-  await computersListPage.goto();
-  await computersListPage.searchBy("noData");
-  await computersListPage.verifyNoData();
-});
-
-//task 4 add two more tests to search feature, move them all to same group
-
-test.describe("Task 4: Verify search feature", async () => {
-  test("Verify search feature one match", async ({page}) => {
-    let computersListPage = new ComputersListPage(page);
-    await computersListPage.goto();
-    await computersListPage.searchBy("Asci White");
-    await computersListPage.verifyData("One computer found");
-  })
-test("Verify search feature with multiple matches", async ({page}) => {
-  let computersListPage = new ComputersListPage(page);
-  await computersListPage.goto();
-  await computersListPage.searchBy("Lenovo");
-  await computersListPage.verifyData("19 computers found");
-})
-
-test("Verify search feature with no data", async ({page}) => {
-  let computersListPage = new ComputersListPage(page);
-  await computersListPage.goto();
-  await computersListPage.searchBy("noData");
-  await computersListPage.verifyNoData();
-});
-
-})
