45e0b63108638d7f25eedf040e206fdb0f1cfccc
Lurdė Andrijauskaitė <Lurde@Lurdes-MacBook-Air-2.local>
2024-08-04 18:23:36 +0300

    Tikiuosi visi testai bus aiškūs, nežinau, ar visi teisingi, ar tikrina tai, ką turi tikrinti, arba toje vietoje, kurioje turi tikrinti. Tikiuosi. Yra labai daug dalykų, kuriuos galima būtų tikrinti pagal pateiktas užduotis, siekiant tikslo - jog puslapis veiktų gerai, tačiau visų testų ir visų kampų nesurašiau. Pažymėjau tuos, kurie man atrodė taikliausiai patikrina tai, kas prašoma pagal užduočių tekstus.

--- 1_unit/tests/calculate-score-range.test.js
+++ 1_unit/tests/calculate-score-range.test.js
@@ -12,4 +12,19 @@ describe("calculateScoreRange tests", () => {
   test("5, 25, 25 numbers should get range 3", () => {
     expect(calculateScoreRange(5, 25, 25)).toBe(3);
   });
+
+  test("0, 0, 0 numbers should get range 1", () => {
+    expect(calculateScoreRange(0, 0, 0)).toBe(1); });
+
+  test("10, 4, 3 numbers should get range 1", () => {
+    expect(calculateScoreRange(10, 4, 3)).toBe(1); });
+
+  test("9, 5, 4 numbers should get range 1", () => {
+    expect(calculateScoreRange(9, 5, 4)).toBe(2); });
+      
+  test("11, 29, 10 numbers should get range 1", () => {
+    expect(calculateScoreRange(11, 29, 10)).toBe(2);});
+
+  test("26, 15, 10 numbers should get range 1", () => {
+    expect(calculateScoreRange(26, 15, 10)).toBe(2);});
 });
---
+++ 3_e2e/pages/add-computer-page.js
@@ -0,0 +1,12 @@
+import { expect } from "@playwright/test";
+import { name } from "../playwright.config";
+
+export class AddComputerPage {
+  constructor(page) {
+    this.page = page;
+  }
+
+  async addComputerName(name) {
+    await this.page.locator('#name').fill(name);
+  }
+
