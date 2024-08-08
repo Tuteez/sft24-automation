0972767dee70f4127242f50c59d0693630700931
Ola0191 <ajurkowska0191@gmail.com>
2024-08-08 19:42:57 +0200

    tests added

--- 4_homework/tests/products-list.spec.js
+++ 4_homework/tests/products-list.spec.js
@@ -17,3 +17,22 @@ test('Sort products by name in ascending order', async ({ page }) => {
   let isSorted = await productsListPage.isListSortedByName(true);
   expect(isSorted).toBe(true);
 });
+
+test('Sort products by name in descending order', async ({ page }) => {
+  let productsListPage = new ProductsListPage(page);
+  await productsListPage.selectSortAction('za');
+  let isSorted = await productsListPage.isListSortedByName(false);
+  expect(isSorted).toBe(true);
+});
+test('Sort products by price in ascending order', async ({ page }) => {
+  let productsListPage = new ProductsListPage(page);
+  await productsListPage.selectSortAction('lohi');
+  let isSorted = await productsListPage.isListSortedByName(true);
+  expect(isSorted).toBe(true);
+});
+test('Sort products by price in descending order', async ({ page }) => {
+  let productsListPage = new ProductsListPage(page);
+  await productsListPage.selectSortAction('hilo');
+  let isSorted = await productsListPage.isListSortedByName(false);
+  expect(isSorted).toBe(true);
+});
