7b507b2d2da9e4908fc5eedcb18c9d1cede5312f
beagas <beatrice.gaskiene@gmail.com>
2024-08-09 15:46:39 +0300

    updated homework after consultation

---
+++ 4_homework/data/sorting_test_data.js
@@ -0,0 +1,35 @@
+export const sortingOptions = {
+    nameAToZ: {
+        dropdownOption: "Name (A to Z)",
+        expectedSorting: {
+            sortedBy: "name",
+            ascending: true,
+        } 
+    },
+    nameZToA: {
+        dropdownOption: "Name (Z to A)",
+        expectedSorting: {
+            sortedBy: "name",
+            ascending: false,
+        }
+    },
+    priceLowToHigh: {
+        dropdownOption: "Price (low to high)",
+        expectedSorting: {
+            sortedBy: "price",
+            ascending: true,
+        }
+    },
+    priceHighToLow: {
+        dropdownOption: "Price (high to low)",
+        expectedSorting: {
+            sortedBy: "price",
+            ascending: false,
+        }
+    }
+}
+
+export const sortingOptionValues = Object.values(sortingOptions)
+
+
+
